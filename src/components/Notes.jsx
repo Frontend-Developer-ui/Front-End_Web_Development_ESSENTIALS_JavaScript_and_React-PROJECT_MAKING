1c28f6a58d886ef505f36dcd885142d323bedb57
MRITYUNJAY_YADAV <93583572+ANTRUMEYE@users.noreply.github.com>
2022-12-04 17:17:42 +0530

    Initial commit

---
+++ src/components/Notes.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Notes() {
+  return (
+    <div className="note">
+      <h1>Deep Learning</h1>
+      <p>
+        All the students who complete the Bootcamp with complete attendance and
+        project will be rewarded certificates from us..
+      </p>
+    </div>
+  );
+}
+
+export default Notes;
